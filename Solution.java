6a0124d265f0b67265ce08482f78155a2b99fb9a
Yogirajkhalate <yogiraj9696khalate@gmail.com>
2024-12-20 21:05:20 +0530

    first commit

---
+++ Solution.java
@@ -0,0 +1,41 @@
+/*Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].*/
+
+import java.util.Arrays;
+
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int n = nums.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[] { i, j };
+                }
+            }
+        }
+        return new int[] {};
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = { 2, 7, 11, 15 };
+        int target = 9;
+        int[] result = solution.twoSum(nums, target);
+        if (result.length > 0) {
+            System.out.println("Indices: " + Arrays.toString(result));
+        } else {
+            System.out.println("No solution found!");
+        }
+    }
+}
